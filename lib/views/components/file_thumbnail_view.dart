5a62466c74b2bcdbb12d617ee8a22d03197c5dfe
kaldillard <mdillard@ferrum.edu>
2023-06-12 20:11:49 -0400

    thumbnail view

---
+++ lib/views/components/file_thumbnail_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagram_clone/state/image_upload/models/thumbnail_request.dart';
+import 'package:instagram_clone/state/image_upload/providers/thumbnail_provider.dart';
+import 'package:instagram_clone/views/components/animations/loading_animation_view.dart';
+import 'package:instagram_clone/views/components/animations/small_error_animation_view.dart';
+
+class FileThumbnailView extends ConsumerWidget {
+  final ThumbnailRequest thumbnailRequest;
+
+  const FileThumbnailView({
+    super.key,
+    required this.thumbnailRequest,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final thumbnail = ref.watch(thumbnailProvider(
+      thumbnailRequest,
+    ));
+    return thumbnail.when(
+      data: (imageWithAspectRatio) {
+        return AspectRatio(
+          aspectRatio: imageWithAspectRatio.aspectRatio,
+          child: imageWithAspectRatio.image,
+        );
+      },
+      loading: () {
+        return const LoadingAnimationView();
+      },
+      error: (context, stackTrace) {
+        return const SmallErrorAnimationView();
+      },
+    );
+  }
+}
