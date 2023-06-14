46fb1b0565968fcdf373d24de3968ffba6a1f038
kaldillard <mdillard@ferrum.edu>
2023-06-13 22:14:06 -0400

    upload buttons completed

--- lib/state/providers/is_loading_provider.dart
+++ lib/state/providers/is_loading_provider.dart
@@ -1,7 +1,8 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:instagram_clone/state/auth/providers/auth_state_provider.dart';
+import 'package:instagram_clone/state/image_upload/typedefs/is_loading.dart';
 
-final isLoadingProvider = Provider<bool>((ref) {
+final isLoadingProvider = Provider<IsLoading>((ref) {
   final authState = ref.watch(authStateProvider);
 
   return authState.isLoading;
