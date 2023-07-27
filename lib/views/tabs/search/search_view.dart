560c69b231b51156311b3ec9e78db51bf44f103b
kaldillard <mdillard@ferrum.edu>
2023-07-27 16:50:30 -0400

    done with search

--- lib/views/tabs/search/search_view.dart
+++ lib/views/tabs/search/search_view.dart
@@ -20,26 +20,26 @@ class SearchView extends HookConsumerWidget {
       );
     }, [controller]);
 
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            controller: controller,
-            textInputAction: TextInputAction.search,
-            decoration: InputDecoration(
-                labelText: Strings.enterYourSearchTermHere,
-                suffixIcon: IconButton(
-                    onPressed: () {
-                      controller.clear();
-                      dismissKeyboard();
-                    },
-                    icon: const Icon(Icons.clear))),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: controller,
+              textInputAction: TextInputAction.search,
+              decoration: InputDecoration(
+                  labelText: Strings.enterYourSearchTermHere,
+                  suffixIcon: IconButton(
+                      onPressed: () {
+                        controller.clear();
+                        dismissKeyboard();
+                      },
+                      icon: const Icon(Icons.clear))),
+            ),
           ),
         ),
-        Expanded(
-          child: SearchGridView(searchTerm: searchTerm.value),
-        )
+        SearchGridView(searchTerm: searchTerm.value),
       ],
     );
   }
